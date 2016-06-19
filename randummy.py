bf53f91e55c6258a9483481bf5866c1823b49a35
jrbattles <jason.r.battles@gmail.com>
2016-06-18 22:45:01 -0500

    more edits

--- randummy.py
+++ randummy.py
@@ -1,11 +1,15 @@
 def compMatch():
-    print("You guessed {}.".format(userNum))
+    print("You guessed {}.".format(int(userNum)))
+    #print("myNum = {}.".format(int(myNum)))
     if userNum == myNum:
         print("Wow!  You guessed correctly!!!")
     else:
         print("Nope.  You guessed incorrectly.  Please try again")
-        compMatch()
-
+        
 import random
 myNum = random.randint(1,10)
-compMatch
+# print(myNum)
+userNum = input("Guess a Number between 1 and 10: ")
+userNum = int(userNum)
+# print("userNum = {}.".format(int(userNum)))
+compMatch()
